05c217c5e9bf67e3ddb4c1f5e7bab5f70baaf8e7
sandrapalakkal <sandrakk011@gmail.com>
2023-06-21 17:07:11 +0530

    file

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import reactLogo from '../assets/pic.png'
+import './Register.css';
 
 export const Register = () => {
   return (
-    <div>Register Here</div>
+    <div class="col-md-12 text-center">
+      <h3 class="animate-character">Register Here</h3><br/>
+      <img src={reactLogo} className='img' alt="" /></div>
   )
 }
 export default Register
